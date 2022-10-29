34f3cfe015cdda713b9c12c4de55bbc3bc0e6436
Christian Everett <ceverett@brydge.com>
2022-10-29 10:30:14 -0700

    Finished solution

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nodes-challenge
+
+Given a single starting node ID 089ef556-dfff-4ff2-9733-654645be56fe, write an algorithm to retrieve data from the endpoint https://nodes-on-nodes-challenge.herokuapp.com/nodes/:id and traverse the complete node graph in order to answer the 2 following questions:
+
+
+1. What is the total number of unique node IDs?  30
+
+2. What is the most common node ID? "263a3e61-2eae-4dee-a15b-5c0dacad4db4"
---
+++ nodes.rb
@@ -0,0 +1,50 @@
+require 'rest-client'
+require 'json'
+require 'logger'
+
+# Set up logger. The levels are:
+# +UNKNOWN+:: An unknown message that should always be logged.
+# +FATAL+:: An unhandleable error that results in a program crash.
+# +ERROR+:: A handleable error condition.
+# +WARN+::  A warning.
+# +INFO+::  Generic (useful) information about system operation.
+# +DEBUG+:: Low-level information for developers.
+@logger       = Logger.new(STDOUT)
+# @@logger = Logger.new('logfile.log')
+@logger.level = Logger::INFO
+
+
+@id_map = {"089ef556-dfff-4ff2-9733-654645be56fe" => 1}
+
+def get_nodes node_id
+
+  base_url = "https://nodes-on-nodes-challenge.herokuapp.com/nodes"
+  url = "#{base_url}/#{node_id}"
+  @logger.info "Getting #{url}"
+
+  resp = RestClient.get(url)
+  body = JSON.parse(resp.body)
+  
+  body.each do |item|
+    child_nodes = item['child_node_ids']
+    @logger.info "Found #{child_nodes.length} child nodes"
+
+    return nil if child_nodes.nil?
+
+    child_nodes.each do |child_node|
+
+      if @id_map[child_node].nil?
+        @id_map[child_node] = 0
+      end
+
+      @id_map[child_node] += 1
+
+      get_nodes child_node
+    end
+
+  end
+end
+
+node_id = "089ef556-dfff-4ff2-9733-654645be56fe"
+
+get_nodes node_id
